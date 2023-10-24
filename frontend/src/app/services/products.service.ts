24c5a60db2ede80d89281f1b7c1a7ef67bcbac5e
Mohit <mohitcanada2020@gmail.com>
2023-10-24 13:50:17 -0400

    Product details UI

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -13,14 +13,40 @@ export class ProductsService {
     return sample_products;
   }
 
-  getAllPrductsBySearch(searchTerm:string){
-    return this.getAll().filter(product=>product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  getAllPrductsBySearch(searchTerm: string) {
+    return this.getAll().filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   }
 
-  // getProductsById(productId:string){
-  //   return this.getAll().find(prod=>prod.id == productId)?? new Product();
-  // }
-  getProductsById(productId:string){
-    return this.getAll().find(prod=>prod.id == productId)?? new Product();
+  getProductsById(productId: string): Product {
+    return this.getAll().find((prod) => prod.id == productId) ?? new Product();
+  }
+
+  getProductsByCategory(categoryName: string) {
+    return categoryName.toLowerCase() == 'all'
+      ? this.getAll()
+      : this.getAll().filter(
+          (product) =>
+            product.category.toLowerCase() === categoryName.toLowerCase()
+        );
+  }
+
+  loadResources(){
+    var urlParams:any = [];
+    window.location.search.replace("?", "").split("&").forEach(function (e, i) {
+        var p = e.split("=");
+        urlParams[p[0]] = p[1];
+    });
+
+    // We have all the params now -> you can access it by name
+    console.log(urlParams["loaded"]);
+
+    if(urlParams["loaded"]) {}else{
+
+        let win = (window as any);
+        win.location.search = '?loaded=1';
+        //win.location.reload('?loaded=1');
+    }
   }
 }
